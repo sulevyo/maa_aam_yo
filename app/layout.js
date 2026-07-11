fbf9a69dd9a54d275e4a9c7d10d6479eed29608f
Sulev Lion <sulevlion@Mac.lan>
2026-07-12 00:40:23 +0300

    fix: rannaniiittt

--- app/layout.js
+++ app/layout.js
@@ -1,33 +1,12 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+export const metadata = {
+  title: 'Maa.aam',
+  description: 'Original ambient sounds from Estonia',
+}
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Maa.aam",
-  description: "Ambient recordings from Estonia",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="et">
+      <body>{children}</body>
     </html>
-  );
+  )
 }
