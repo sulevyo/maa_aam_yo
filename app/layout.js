8e018d40d9a73d1aab4af8d8c642b2ecf398fbdc
Sulev Lion <sulevlion@Mac.lan>
2026-07-12 00:47:39 +0300

    fix uus

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,29 @@
-import './globals.css';
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Maa.aam',
-  description: 'Original ambient sounds from Estonia',
-}
+  title: "Maa.aam",
+  description: "Original ambients sounds from Estonia",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="et">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
